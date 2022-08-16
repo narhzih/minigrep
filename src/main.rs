b7d87b17dae2ca9c6b288bee51f9df34df62548a
Narhzih <omosekejiolawale@gmail.com>
2022-08-17 00:34:37 +0100

    Removed println statement

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,6 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
     // Read contents of the specified file
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
